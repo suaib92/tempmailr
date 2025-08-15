8670a3ec67e242d533c83d7f19ff3fcd75a78e83
suaib <suaib8211@gmail.com>
2025-08-15 20:29:40 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+// next.config.js - Update to this:
 module.exports = {
   async headers() {
     return [
@@ -6,12 +7,11 @@ module.exports = {
         headers: [
           { 
             key: 'Access-Control-Allow-Origin', 
-            value: process.env.NODE_ENV === 'development' 
-              ? 'http://localhost:3000' 
-              : 'https://www.temp-mailr.com' 
+            value: '*' // Allow all origins in production too
           },
           { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' }
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+          { key: 'Access-Control-Allow-Credentials', value: 'true' }
         ],
       },
     ];
